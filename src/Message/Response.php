7322435aca15972a28c5c1204686cd53a945debd
zqhong <zqh0513@gmail.com>
2018-04-12 10:55:16 +0800

    fixed

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -11,7 +11,27 @@ class Response extends AbstractResponse
      */
     public function isSuccessful()
     {
-        if (isset($this->getData()['return_code']) && $this->getData()['return_code'] === 'SUCCESS') {
+        $data = $this->getData();
+
+        if (
+            is_array($data) &&
+            // return_code，通信标识，取值：SUCCESS/FAIL
+            isset($data['return_code']) &&
+            $data['return_code'] === 'SUCCESS' &&
+
+            // result_code，业务结果，取值：SUCCESS/FAIL
+            isset($data['result_code']) &&
+            $data['result_code'] === 'SUCCESS'
+        ) {
+            return true;
+        }
+
+        // 对账单下载
+        // 成功返回是纯文本
+        if (
+            $this->getRequest() instanceof DownloadStatementRequest &&
+            is_string($data)
+        ) {
             return true;
         }
 
